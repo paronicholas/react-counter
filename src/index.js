2852dc71a874a1685605dbd9098e9edd26528a57
paronicholas <31823783+paronicholas@users.noreply.github.com>
2019-07-16 10:21:38 -0700

    removed header
    removed the pre-made header function

--- src/index.js
+++ src/index.js
@@ -3,19 +3,9 @@ import ReactDOM from "react-dom";
 import Counter from "./app.js";
 import "./styles.css";
 
-function Header() {
-  return (
-    <React.Fragment>
-      <h1>Welcome to the Counter</h1>
-      <p>Press the button below to see what happens</p>
-    </React.Fragment>
-  );
-}
-
 function App() {
   return (
     <div className="App">
-      <Header />
       <Counter />
     </div>
   );
